fa459a22967f63987c5d84d68d2c77a42c40ddc7
tpo <tpo>
2004-06-01 08:53:32 +0000

    added automake version check

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+( automake --version | head -1 | grep -q ' 1\.[678]') || (echo "automake 1.6 or above is required"; exit 1)
 aclocal -I acinclude     && \
 autoheader               && \
 automake -a              && \
