a5394e87cab2fa4715c865797ad738cf7a608c8e
tpo <tpo>
2004-08-20 08:11:44 +0000

    ignore stamp-in, signal to gentoo which autobuild to use (both by Michael), bump version

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-( automake --version | head -1 | grep -q ' 1\.[678]') || (echo "automake 1.6 or above is required"; exit 1)
+
+# needed for Gentoo to choose the right automake
+WANT_AUTOMAKE=1.8
+export WANT_AUTOMAKE
+
+( automake --version | head -n 1 | grep -q ' 1\.[678]') || (echo "automake 1.6 or above is required"; exit 1)
 aclocal -I acinclude     && \
 autoheader               && \
 automake -a              && \
